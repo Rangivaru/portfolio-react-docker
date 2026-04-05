0d44ec1f839d12dcf3ff35a96c1be3955ab3e0ea
Rangivaru <salem.rangivaru@gmail.com>
2026-04-05 22:27:11 +0200

    feat: update projects data to reflect new portfolio details and remove unused technology references (#2)

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -16,29 +16,23 @@ const FIGMA_COLOR = "#60CC89";
 const NEXT_COLOR = "#FFFFFF";
 const NODE_COLOR = "#669C4F";
 const REACT_COLOR = "#62D5FA";
-const STORYBOOK_COLOR = "#E65482";
-const STRIPE_COLOR = "#995DF6";
-const SPOTIFY_COLOR = "#48CB65";
 
 const REACT_NAME = "ReactJS";
 const NEXT_NAME = "NextJS";
 const NODE_NAME = "NodeJS";
 const FIGMA_NAME = "Figma";
-const STORYBOOK_NAME = "Storybook";
-const API_SPOTIFY = "Spotify API";
-const STRIPE_NAME = "Stripe";
-
 export const projects: ProjectsDTO[] = [
     {
         id: 1,
-        year: 2024,
-        title: "Musical Guess",
-        description: "Quiz musical sur les artistes et les chansons utilisant l'API Spotify.",
-        href: "https://musicalguess.jeremiebarriere.fr",
+        year: 2025,
+        title: "Portfolio",
+        description: "Portfolio personnel présentant mes projets et compétences.",
+        href: "https://portfolio-react-docker-9sjyi0ybf-rangivarus-projects.vercel.app",
         technos: [
             {color: NEXT_COLOR, label: NEXT_NAME},
+            {color: REACT_COLOR, label: REACT_NAME},
+            {color: NODE_COLOR, label: NODE_NAME},
             {color: FIGMA_COLOR, label: FIGMA_NAME},
-            {color: SPOTIFY_COLOR, label: API_SPOTIFY},
         ],
     }
 ];
